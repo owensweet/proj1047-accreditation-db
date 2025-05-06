04b9418fece45be1f8c715ca534eaaffecffb3a2
Owen Sweet <o.sweet.work@gmail.com>
2025-05-06 16:21:05 -0700

    Added frontend folder with static folder for images and css, and templates for html files (django templates). Connected the templates route in settings.py and the home view in views.py.

--- backend/bcit_accreditation/urls.py
+++ backend/bcit_accreditation/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accreditation.views import home #this one we added
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
